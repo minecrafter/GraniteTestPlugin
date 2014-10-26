ba26148ed1866c95fdbdd2d38571c25bdfc4245d
Tux <write@imaginarycode.com>
2014-10-25 21:33:15 -0400

    javadoc

--- src/main/java/io/minimum/minecraft/granitetest/GraniteTestHandler.java
+++ src/main/java/io/minimum/minecraft/granitetest/GraniteTestHandler.java
@@ -5,9 +5,21 @@ import org.granitemc.granite.api.chat.ChatComponentBuilder;
 import org.granitemc.granite.api.event.On;
 import org.granitemc.granite.api.event.player.EventPlayerJoin;
 
+/**
+ * This class contains our event handlers. There is just one simple one.
+ */
 public class GraniteTestHandler {
+    /**
+     * This is our {@link org.granitemc.granite.api.event.player.EventPlayerJoin} handler. It must be annotated with
+     * {@link org.granitemc.granite.api.event.On} so Granite can register it.
+     *
+     * For simplicity, we are sending a message via Granite's chat component builder.
+     *
+     * @param event the event, this is required.
+     */
     @On(event = EventPlayerJoin.class)
     public void onEventPlayerJoin(EventPlayerJoin event) {
+        // This will send the actual message.
         event.getPlayer().sendMessage(new ChatComponentBuilder().text("Oow, that hurts!!!!").color(ChatColor.RED).build());
     }
 }
--- src/main/java/io/minimum/minecraft/granitetest/GraniteTestPlugin.java
+++ src/main/java/io/minimum/minecraft/granitetest/GraniteTestPlugin.java
@@ -4,10 +4,24 @@ import org.granitemc.granite.api.Granite;
 import org.granitemc.granite.api.plugin.Plugin;
 import org.granitemc.granite.api.plugin.PluginContainer;
 
+/**
+ * This class contains the plugin.
+ *
+ * It requires an {@link org.granitemc.granite.api.plugin.Plugin} annotation so that Granite knows to load it.
+ *
+ * Unlike Bukkit, however, you are expected to initialize data state in your constructor.
+ */
 @Plugin(name = "GraniteTestPlugin", id = "testplugin", version = "0.1")
 public class GraniteTestPlugin {
+    /**
+     * The constructor allows us to initialize our state.
+     *
+     * @param container a {@link org.granitemc.granite.api.plugin.PluginContainer}, which will be passed with this. This parameter is not required.
+     */
     public GraniteTestPlugin(PluginContainer container) {
         Granite.getLogger().info("i can haz controls");
+
+        // Register our event handler.
         container.registerEventHandler(new GraniteTestHandler());
     }
 }
